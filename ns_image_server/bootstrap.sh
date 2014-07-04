cffa25bf219401512c457a64b48dbe47aa7ceda2
Zach Pincus <zpincus@gmail.com>
2014-07-04 01:41:56 -0500

    fix bootstrap script; distribute automake files
    Changed bootstrap script to make copies of autotools scripts rather
    than links, so that ./configure will have a better chance of working
    out-of-the-box. Now I think it's OK to include in the git repo the
    files generated by bootstrap.sh.

--- ns_image_server/bootstrap.sh
+++ ns_image_server/bootstrap.sh
@@ -2,5 +2,5 @@
 rm -r -f compile config.guess config.sub depcomp missing install-sh INSTALL autom4te.cache aclocal.m4
 aclocal
 autoheader
-automake  --force-missing --add-missing
+automake  --force-missing --add-missing --copy
 autoconf

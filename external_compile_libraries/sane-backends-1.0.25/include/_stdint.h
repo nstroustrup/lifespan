ee0c1384ef19365671f8cc5c516d669f0242b564
Nicholas Stroustrup <nstroustrup@gmail.com>
2016-11-11 14:46:02 -0500

    fix scan historesis bug by upgrading to new sane backend

---
+++ external_compile_libraries/sane-backends-1.0.25/include/_stdint.h
@@ -0,0 +1,9 @@
+#ifndef _SANE_BACKENDS_INCLUDE__STDINT_H
+#define _SANE_BACKENDS_INCLUDE__STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "sane-backends 1.0.25"
+/* generated using gnu compiler gcc (GCC) 4.4.7 20120313 (Red Hat 4.4.7-17) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif

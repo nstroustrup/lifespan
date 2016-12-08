a79eafd37ae431a87ca764f8e2918bd7228e8f7d
Nicholas Stroustrup <nstroustrup@users.noreply.github.com>
2016-12-08 18:00:45 -0500

    web interface update

--- website/image_server_web/manage_file_deletion_jobs.php
+++ website/image_server_web/manage_file_deletion_jobs.php
@@ -4,6 +4,9 @@ require_once('ns_experiment.php');
 require_once('ns_processing_job.php');
 $refresh=FALSE;
 $job_id = $_POST["job_id"];
+if(array_key_exists("show",$query_string))
+  $show = $query_string["show"];
+else $show="none";
 if ($_POST["confirm"] != ''){
   $query = "UPDATE delete_file_jobs SET confirmed=1 WHERE id=$job_id";
   $sql->send_query($query);
@@ -61,12 +64,18 @@ if ($refresh){
   header("Location: manage_file_deletion_jobs.php\n\n");
   die("");
  }
+if ($show != "none"){
 $query = "SELECT id, confirmed, parent_job_id FROM delete_file_jobs ORDER BY confirmed ASC";
 $sql->get_row($query,$jobs);
+
 for ($i = 0; $i < sizeof($jobs); $i++){
   $query = "SELECT relative_directory,filename FROM delete_file_specifications WHERE delete_job_id=" . $jobs[$i][0];
+  if ($show != "full")
+    $query .= " LIMIT 1";
   $sql->get_row($query,$job_specifications[$i]);
  }
+}
+else $jobs=array();
 
 display_worm_page_header("Manage File Deletion Jobs");
 ?>
@@ -74,11 +83,15 @@ display_worm_page_header("Manage File Deletion Jobs");
 <table border=0 bgcolor="#FFFFFF" cellspacing=0 cellpadding=3>
 <tr><td colspan = 2<?php echo $table_header_color?>>File Deletion Jobs Pending Confirmation<br>
 <form method="post" action="manage_file_deletion_jobs.php">
-<?php if (sizeof($jobs) > 0){?>
-<input name="dismiss_all" type="submit" value="Dismiss All Deletion Requests" onClick="javascript:return confirm('Are you sure you wish to dismiss all requests?')"><?php }?>
+<input name="dismiss_all" type="submit" value="Dismiss All Deletion Requests" onClick="javascript:return confirm('Are you sure you wish to dismiss all requests?')">
 </form>
+ <?php if ($show == "partial") 
+echo "<font size=-1><a href=\"manage_file_deletion_jobs.php?show=full\">[Show more detail]</a></font>";?>
 </td></tr><?php
-if (sizeof($jobs) == 0){
+if ($show == "none"){
+echo "<tr><td  bgcolor=\"{$table_colors[0][0]}\"><a href=\"manage_file_deletion_jobs.php?show=partial\">Click here to show pending deletion requests</a></td></tr>\n";
+}
+else if (sizeof($jobs) == 0){
   echo "<tr><td  bgcolor=\"{$table_colors[0][0]}\">No Deletion Jobs are Ready for Processing.</td></tr>\n";
 }
  else{
@@ -108,7 +121,7 @@ if (sizeof($jobs) == 0){
      for ($j=0; $j < $max; $j++)
        echo $job_specifications[$i][$j][0] . "\\" . $job_specifications[$i][$j][1] . "<BR>\n";
 	if ($s != $max)
-		echo "<center>...<br><a href=\"manage_file_deletion_jobs.php?show_details_id={$job_id}#id{$job_id}\">[View More]</a><center>";
+		echo "<center>...<br><a href=\"manage_file_deletion_jobs.php?show=" . $show . "&show_details_id={$job_id}#id{$job_id}\">[View More]</a><center>";
      echo "</td><td bgcolor=\"{$table_colors[($i+1)%2][1]}\" valign=\"top\">";
      if ($jobs[$i][1] == "0")
        echo "<form method=\"post\" action=\"manage_file_deletion_jobs.php\"><input type=\"submit\" name=\"confirm\" value=\"Confirm\"><br><input type=\"submit\" name=\"dismiss\" value=\"Dismiss\"><input type=\"hidden\" name=\"job_id\" value=\"" . $job_id . "\"></form>";
@@ -119,8 +132,7 @@ if (sizeof($jobs) == 0){
  }
  ?><tr><td colspan = 2<?php echo $table_header_color?>>
 <form method="post" action="manage_file_deletion_jobs.php">
-<?php if (sizeof($jobs) > 0){?>
-<input name="confirm_all" type="submit" value="Confirm All Deletion Requests" onClick="javascript:return confirm('Are you sure you wish to confirm all deletion requests?  This is pretty dangerous.')"><?php }?>
+<input name="confirm_all" type="submit" value="Confirm All Deletion Requests" onClick="javascript:return confirm('Are you sure you wish to confirm all deletion requests?  This is pretty dangerous.')">
 </form>
 </td></tr>
 </table></td></tr></table><br>

1306035181782c68da99585a8d55ebe86cd4c085
Nicholas Stroustrup <nstroustrup@gmail.com>
2018-05-18 16:40:09 +0200

    web interface update

--- web_interface/image_server_web/view_scanner_schedule.php
+++ web_interface/image_server_web/view_scanner_schedule.php
@@ -8,7 +8,10 @@ $experiment_id = @$query_string['experiment_id'];
 $device_name = @$query_string['device_name'];
 $show_past = @$query_string['show_past'];
 $show_future = @$query_string['show_future'];
-
+$show_cancelled = @$query_string['show_cancelled'];
+if (!isset($show_cancelled))
+   $show_cancelled = FALSE;
+else $show_cancelled = $show_cancelled=="1";
 $experiment = new ns_experiment($experiment_id,"",$sql);
 if ($experiment->name == '')
 	$experiment->name = "All";
@@ -41,7 +44,7 @@ function display_double_hash_name($id1,$id2,&$name_hash){
 	return $name_hash[$id1][$id2];
 
 }
-function out_link($ex_id='',$dev_name='', $show_p ='',$show_f=''){
+function out_link($ex_id='',$dev_name='', $show_p ='',$show_f='',$show_cancelled=true){
   global $experiment_id,$device_name,$show_past,$show_future; 
  if ($ex_id == '')
     $ex_id = $experiment_id;
@@ -58,32 +61,34 @@ function out_link($ex_id='',$dev_name='', $show_p ='',$show_f=''){
     $str.="experiment_id=$ex_id&";
   if ($dev_name != '')
     $str.="device_name=$dev_name&";
+    if ($show_cancelled)
+    $str.="show_cancelled=1&";
   $str.= "show_past=$show_p&show_future=$show_f\">";
   return $str;
 }
 
-function out_limits_p(&$event_type,$limits){
+function out_limits_p(&$event_type,$limits,$show_cancelled){
   if ($event_type == 0)
-    return out_link('','',$limits,'');
-  else return out_link('','','',$limits);
+    return out_link('','',$limits,'',$show_cancelled);
+  else return out_link('','','',$limits,$show_cancelled);
 }
-function out_limits($event_type,&$data,$limits,$N){ 
+function out_limits($event_type,&$data,$limits,$N,$show_cancelled){ 
   if ($limits[0] != 0){
     $lim = $limits[0] - 50;
     if ($lim < 0)
       $lim = 0;
-    $out = out_limits_p($event_type, array($lim,min($lim+50,$N))) . "[prev]</a>";
+    $out = out_limits_p($event_type, array($lim,min($lim+50,$N)),$show_cancelled) . "[prev]</a>";
   }
   else $out = "[prev]";
   if ($limits[1] - $limits[0] < $N){
      if ($limits[1] - $limits[0] < 50)
-     $out .= out_limits_p($event_type,array($limits[0],min($limits[0]+50,$N))); 
-   else $out .= out_limits_p($event_type,array($limits[0]+50,min($limits[1]+50,$N)));
+     $out .= out_limits_p($event_type,array($limits[0],min($limits[0]+50,$N)),$show_cancelled); 
+   else $out .= out_limits_p($event_type,array($limits[0]+50,min($limits[1]+50,$N)),$show_cancelled);
 $out .= " [next]</a>";
 }
   else $out .= "[next]";
 if ($limits[1] - $limits[0] < $N)
-  $out .= out_limits_p($event_type,array(0,$N))."[Show all]</a>";
+  $out .= out_limits_p($event_type,array(0,$N),$show_cancelled)."[Show all]</a>";
   return "Showing events " . ($limits[0]+1) . "-" . $limits[1] . " out of " . $N . "<br>" . $out;
 }
 
@@ -99,7 +104,8 @@ function display_events(&$events,&$name_hash,&$sample_hash,&$limits){
 	for ($i = 0; $i < sizeof($events); $i++){
 		$clrs = $table_colors[$i%2];
 	
-		echo "<tr align=\"center\"><td bgcolor=\"$clrs[0]\">".format_time($events[$i][4]). " " . "</td>".
+		echo "<tr align=\"center\"><td bgcolor=\"$clrs[0]\">";
+ echo format_time($events[$i][4]). " " . "</td>".
 		  "<td bgcolor=\"$clrs[1]\">";  if ($events[$i][3] == "0") echo "(none)"; else echo $events[$i][3]; echo "</td>".
 			"<td bgcolor=\"$clrs[0]\">". display_hash_name($events[$i][1],$name_hash)."." . display_double_hash_name($events[$i][1],$events[$i][2],$sample_hash) ."</td>".
 			"<td bgcolor=\"$clrs[1]\">".format_time($events[$i][5])."</td>".
@@ -109,6 +115,9 @@ function display_events(&$events,&$name_hash,&$sample_hash,&$limits){
 			else if ($events[$i][5] != 0)echo "<i>in progress</i>";
 			echo "</td>";
 		echo "<td bgcolor=\"$clrs[1]\">";
+		
+                if ($events[$i][9] != 0)
+                echo "<i>(Cancelled by user)</i>";
 		if ($events[$i][7] != 0)
 		echo "<center> <a href=\"view_hosts_log.php?event_id=".$events[$i][7]."\">(Problem)</a></center>";
 		echo "</td>".
@@ -154,7 +163,7 @@ for ($i = 0; $i < sizeof($res); $i++)
 ns_generate_sample_hash($experiment_id,$sample_names, $sql);
 
 $info = "SELECT capture_schedule.id, capture_schedule.experiment_id, capture_schedule.sample_id, capture_samples.device_name, capture_schedule.scheduled_time, "
-      . "capture_schedule.time_at_start, capture_schedule.time_at_finish, capture_schedule.problem, capture_schedule.captured_image_id FROM capture_schedule, capture_samples " ;
+      . "capture_schedule.time_at_start, capture_schedule.time_at_finish, capture_schedule.problem, capture_schedule.captured_image_id, capture_schedule.censored FROM capture_schedule, capture_samples " ;
 $postfix_ASC = "";
 for ($i = 0; $i < sizeof($sql_clauses); $i++){
 	$postfix_ASC .= " AND " . $sql_clauses[$i];
@@ -169,15 +178,22 @@ $postfix_DESC .= $str;
 
 $counting_prefix = "SELECT count(capture_schedule.id) FROM capture_schedule, capture_samples ";
 if ($show_future[1] - $show_future[0] > 0){
-  $suffix = " WHERE $experiment_mysql capture_schedule.scheduled_time >= '" . time() . "' AND capture_schedule.time_at_start = 0 $postfix_ASC ";
+  $suffix = " WHERE ";
+  if (!$show_cancelled)
+   $suffix .="capture_schedule.censored=0 AND ";
+  $suffix .= "$experiment_mysql capture_schedule.scheduled_time >= '" . time() . "' AND capture_schedule.time_at_start = 0 $postfix_ASC ";
   $query = $info . $suffix . " LIMIT " . $show_future[0] . "," . $show_future[1];
-  	$sql->get_row($query,$future_events);
+//	echo $query; 
+ 	$sql->get_row($query,$future_events);
 	$query = $counting_prefix . $suffix;
 	$sql->get_value($query,$future_events_count);
 }
 
 
-$suffix = "WHERE $experiment_mysql capture_schedule.time_at_start != 0 AND capture_schedule.time_at_finish = 0 AND capture_schedule.scheduled_time >= " . (time()-60*4) . " $postfix_ASC";
+$suffix = "WHERE ";
+if (!$show_cancelled)
+   $suffix .="capture_schedule.censored=0 AND ";
+$suffix .=" $experiment_mysql capture_schedule.time_at_start != 0 AND capture_schedule.time_at_finish = 0 AND capture_schedule.scheduled_time >= " . (time()-60*4) . " $postfix_ASC";
 //_die($query);
 $query = $info . $suffix;
 $sql->get_row($query,$current_events);
@@ -191,7 +207,10 @@ $query = $counting_prefix . $suffix;
 $sql->get_value($query,$delayed_events_count);
 
 if ($show_past[1] - $show_past[0] > 0){
-	$suffix = " WHERE $experiment_mysql capture_schedule.time_at_finish != 0 $postfix_DESC ";
+	$suffix = " WHERE ";
+	if (!$show_cancelled)
+ 	   $suffix .="capture_schedule.censored=0 AND ";
+	$suffix .= "$experiment_mysql capture_schedule.time_at_finish != 0 $postfix_DESC ";
 	$query = $info . $suffix ."LIMIT " .$show_past[0] . "," . $show_past[1];
        
 	$sql->get_row($query,$completed_events);
@@ -206,7 +225,11 @@ if ($show_past[1] - $show_past[0] > 0){
 display_worm_page_header("Capture Schedule (" . $experiment->name .")");
 
 ?>
-
+<?php echo out_link($experiments[$i][0],$device_name,$show_past,$show_future,!$show_cancelled);
+if ($show_cancelled)
+	echo "[Hide Cancelled Scans]";
+else echo "[Show Cancelled Scans]";
+?>
 <a href="#Current">[Current]</a> <a href="#future">[Future]</a> <a href="#completed">[Completed]<br><br>
 
 
@@ -238,9 +261,10 @@ display_events($current_events, $experiment_names,$sample_names,$device_names);
 if ($show_future[1] - $show_future[0] > 0)
   $r = array(0,0);
  else $r = array(0,50);
-echo  "<br><h2>" . out_link($experiment_id, $device_name, $show_past, $r) . "Future Scans</a></h2>";
+echo  "<br><h2>" . out_link($experiment_id, $device_name, $show_past, $r,$show_cancelled) . "Future Scans</a></h2>";
 if ($show_future[1] - $show_future[0] != 0){
-  echo out_limits(1,$future_events,$show_future,$future_events_count);
+
+  echo out_limits(1,$future_events,$show_future,$future_events_count,$show_cancelled);
   display_events($future_events, $experiment_names,$sample_names,$show_future);
     
     }
@@ -250,7 +274,7 @@ if ($show_past[1] - $show_past[0] > 0)
  else $r = array(0,50);
 echo  "<br><h2>" . out_link($experiment_id, $device_name, $r, $show_future) . "Completed Scans</a></h2>";
 if ($show_past[1] - $show_past[0] > 0){
-  echo out_limits(0,$completed_events,$show_past,$completed_events_count);
+  echo out_limits(0,$completed_events,$show_past,$completed_events_count,$show_cancelled);
   display_events($completed_events, $experiment_names,$sample_names,$show_past);
     }
 

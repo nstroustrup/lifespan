f7332f0e6b18415fc305b2c7c75772fa1b6c91a9
Nicholas Stroustrup <nstroustrup@gmail.com>
2018-07-27 18:27:06 +0200

    bugfix

--- web_interface/image_server_web/ns_sql.php
+++ web_interface/image_server_web/ns_sql.php
@@ -7,7 +7,7 @@
 			$this->name = $sql_name;
 			$this->pwd = $sql_pwd;
 			$this->db = $sql_db;
-			echo $sql_db;
+			//echo $sql_db;
 			$this->link = mysqli_connect($this->server, $this->name, $this->pwd, $this->db);
 			if ($this->link === FALSE) 
 			  throw new ns_exception("Could not connect : " . mysqli_error($this->link));

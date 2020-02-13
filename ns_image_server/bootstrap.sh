22ea049b37011d38655a050204f3e1c63acba206
Nicholas Stroustrup <nstroustrup@gmail.com>
2020-02-13 11:31:01 +0100

    port v1 to cmake

--- ns_image_server/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm -r -f compile config.guess config.sub depcomp missing install-sh INSTALL autom4te.cache aclocal.m4
-aclocal
-autoheader
-automake  --force-missing --add-missing --copy
-autoconf

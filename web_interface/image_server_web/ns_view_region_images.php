f790a5015567884fca0cf2a3a7b821d698754106
Nicholas Stroustrup <nstroustrup@gmail.com>
2021-02-02 19:42:07 +0100

    allow censoring of images via web interface

--- web_interface/image_server_web/ns_view_region_images.php
+++ web_interface/image_server_web/ns_view_region_images.php
@@ -5,13 +5,30 @@ require_once('ns_dir.php');
 require_once('ns_experiment.php');
 require_once('ns_processing_job.php');
 
-
 try{
   $experiment_id = @$query_string['experiment_id'];
   $region_id = @$query_string['region_id'];
   $region_image_id = @$query_string['region_image_id'];
   $sort_forwards = @$query_string['sort_forwards']==1;
   
+  if (array_key_exists('censor_after',$query_string)){
+     $censor_after = @$query_string['censor_after'];
+     $query = "SELECT MIN(censored) FROM sample_region_images WHERE capture_time >= $censor_after AND region_info_id = $region_id";
+     $sql->get_value($query,$val);
+     if ($val == 0)
+     	$to_set = 1;
+     else $to_set = 0;
+     $query = "UPDATE sample_region_images SET censored=$to_set WHERE capture_time >= $censor_after AND region_info_id = $region_id";
+   
+     $sql->send_query($query);
+  }
+  if (array_key_exists('exclude',$query_string)){
+     $exclude = @$query_string['exclude'];
+      $query = "UPDATE sample_region_images SET censored= !censored WHERE capture_time = $exclude AND region_info_id = $region_id";
+   
+     $sql->send_query($query);
+  } 
+
   if ($region_image_id != ''){
     $query = "SELECT r.region_info_id, s.experiment_id, s.name FROM sample_region_images as r, sample_region_image_info as i, capture_samples as s WHERE r.id = $region_image_id AND i.id = r.region_info_id AND i.sample_id = s.id";
     $sql->get_row($query,$res);
@@ -49,7 +66,26 @@ try{
   $query .= " ORDER BY capture_time ASC";
   else $query .= " ORDER BY capture_time DESC";
   $sql->get_row($query,$region_images);
-  $job_offset = 5;
+
+$job_offset = 5;
+$op_ids_to_use = array();
+$op_ids_to_use[0] = 1;
+$op_ids_to_use[1] = 0;
+for ($i = 2; $i < $NS_LAST_PROCESSING_JOB; $i++){
+    $op_ids_to_use[$i] = 0;
+    //never show these
+    if ( $i == $ns_processing_tasks['ns_process_add_to_training_set'] ||
+       $i == $ns_processing_tasks['ns_process_analyze_mask'] ||
+       $i == $ns_processing_tasks['ns_process_compile_video'])
+       continue;
+
+    for ($j = 0; $j < sizeof($region_images); $j++)
+    	if ($region_images[$j][$i+$job_offset] != 0){
+	   $op_ids_to_use[$i] = 1;
+	   break;
+	 }	   
+}
+
   $page_title = $experiment_name . "::" .$sample_name . "::" . $region_name;
 }
 catch (ns_exception $ex){
@@ -62,9 +98,6 @@ if ($sort_forwards){
   echo "<a href=\"ns_view_region_images.php?sort_forwards=0&region_id=$region_id&experiment_id=$experiment_id\">[Reverse order by time]</a>";
  }
  else echo "<a href=\"ns_view_region_images.php?sort_forwards=1&region_id=$region_id&experiment_id=$experiment_id\">[Order by time]</a>";
-$id1 = $ns_processing_tasks['ns_process_add_to_training_set'];
-  $id2 = $ns_processing_tasks['ns_process_analyze_mask'];
-  $id3 = $ns_processing_tasks['ns_process_compile_video'];
 if (sizeof($region_images) == 0) echo "(No region images available)";
  else {
    ?>
@@ -76,14 +109,16 @@ if (sizeof($region_images) == 0) echo "(No region images available)";
    <?php
      $j=1;
    for ($i = 1; $i <= $NS_LAST_PROCESSING_JOB; $i++){
-      if ($i == $id1|| $i == $id2 || $i == $id3) continue;
+      if ($i != 1 && $op_ids_to_use[$i-1] == 0) continue;
       if ($i == 1)
 	$name = 'Unprocessed';
       else $name = $ns_processing_task_labels[$i];
       echo "<td bgcolor=\"" . $table_colors[0][$j%2] . "\"><font face=\"Arial\" size=\"-1\"><b>" . $name . "</b></font></td>";
       $j++;
     }
-   ?>
+   echo "<td bgcolor=\"" . $table_colors[0][$j%2] . "\"><font face=\"Arial\" size=\"-1\"><b>Edit";
+   echo  "</b></font></td>";
+?>
      </tr>  
      <?php for ($i = 0; $i < sizeof($region_images); $i++){ 
      ?>
@@ -103,7 +138,7 @@ if (sizeof($region_images) == 0) echo "(No region images available)";
      <?php 
 	 $k = 0;
 	 for ($j = 2; $j <= $NS_LAST_PROCESSING_JOB; $j++){
-	if ($j == $id1|| $j == $id2 || $j == $id3) continue;
+	if ($op_ids_to_use[$j-1] == 0) continue;
 	echo "<td bgcolor=\"{$table_colors[1][$k%2]}\"><center>";
 	
 	if ($region_images[$i][$j + -1 +$job_offset] != 0){
@@ -113,6 +148,11 @@ if (sizeof($region_images) == 0) echo "(No region images available)";
 	echo "</center></td>";
 	$k++;
       }
+       echo "<td bgcolor=\"{$table_colors[1][$k%2]}\"><center>";
+       echo "<font size=\"-1\">";
+       echo "<a href=\"ns_view_region_images.php?region_id=$region_id&experiment_id=$experiment_id&censor_after=".$region_images[$i][1]."\">[Censor_After]</a> ";
+        echo "<a href=\"ns_view_region_images.php?region_id=$region_id&experiment_id=$experiment_id&exclude=".$region_images[$i][1]."\">[Censor_This]</a>";
+       echo "</font></center></td>";
      ?>
      </tr>  
      <?php }?>
